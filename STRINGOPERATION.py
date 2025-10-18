a6573b784729ca1c4e08b1323dce9dce4073104e
jbathuri <jagadeeshbathuri@gmail.com>
2025-10-18 16:05:55 +0530

    string example

---
+++ STRINGOPERATION.py
@@ -0,0 +1,22 @@
+import string
+string_name = "john"
+string_name_2 = "rebecca"
+print(string.digits)
+print(string.ascii_letters)
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
+print(string_name + string_name_2)
+#print(string_name - string_name_2)
+#unspported operand
+#print(string_name * string_name_2)
+#TypeError: can't multiply sequence by non-int of type 'str'
+#print(string_name / string_name_2)
+#TypeError: unsupported operand type(s) for /: 'str' and 'str'
+#print(string_name % string_name_2)
+#TypeError: not all arguments converted during string formatting
+#print(string_name // string_name_2)
+#TypeError: unsupported operand type(s) for //: 'str' and 'str'
+#print(string_name ^ string_name_2)
+#TypeError: unsupported operand type(s) for ^: 'str' and 'str'
+print()
+
